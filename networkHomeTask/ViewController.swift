cc161514668ec95c3603751fb5c62dce16785b83
midniqhtman <c1esi@mail.ru>
2022-11-06 23:12:40 +0300

    Initial Commit

---
+++ networkHomeTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  networkHomeTask
+//
+//  Created by Байсаев Зубайр on 06.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
