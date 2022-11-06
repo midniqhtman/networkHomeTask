a8aebb186ef97356ff38c6be4672e9bffa3bef27
midniqhtman <c1esi@mail.ru>
2022-11-06 23:17:16 +0300

    final

--- networkHomeTask/ViewController.swift
+++ networkHomeTask/ViewController.swift
@@ -12,8 +12,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        categoryLoad()
     }
+        var categories: [String] = []
+        
+        func categoryLoad() {
 
+            let urlString = "https://fakestoreapi.com/products/categories"
+            guard let url = URL(string: urlString) else { return }
+
+            URLSession.shared.dataTask(with: url) { data, response, error in
+        if let error = error {
+            print(error)
+        }
+        
+        guard let data = data else {
+            return
+        }
+            do {
+        let categories = try JSONDecoder().decode([String].self, from: data)
+
+                DispatchQueue.main.async {
+                    self.categories = categories
+                    print(categories)
+                    }
+                } catch { print(error) }
+            }.resume()
+    }
 
 }
 
